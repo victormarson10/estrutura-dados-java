8830d4fbeca90c1a74dd734cde59137639375da6
victor10 <victormarson10@gmail.com>
2023-02-23 19:46:17 -0300

    refactoring class "No" using Generics concept

--- estrutura-dados/src/main/java/com/projetono/Main.java
+++ estrutura-dados/src/main/java/com/projetono/Main.java
@@ -5,15 +5,16 @@ public class Main {
     public static void main(String[] args){
 
         /*no1 -> no2 -> no3 -> no4 -> null*/
-        No no1 = new No("Conteúdo no1");
+        /*Em versoes mais atuais da jdk n tem necessidade de declarar String entre os diamonds da instancia*/
+        No<String> no1 = new No<String>("Conteúdo no1");
 
-        No no2 = new No("Conteúdo no2");
+        No<String> no2 = new No<String>("Conteúdo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<String>("Conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<String>("Conteúdo no4");
         no3.setProximoNo(no4);
 
         System.out.println(no1);
