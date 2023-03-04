60dc4e8105b279ea3943b9b730fd3deca1c6ffc8
victor10 <victormarson10@gmail.com>
2023-03-04 11:49:19 -0300

    implementation of metods to the list chained

--- estrutura-dados/src/main/java/com/listaencadeada/ListaEncadeada.java
+++ estrutura-dados/src/main/java/com/listaencadeada/ListaEncadeada.java
@@ -1,2 +1,99 @@
-package com.listaencadeada;public class ListaEncadeada {
+package com.listaencadeada;
+
+public class ListaEncadeada<T> {
+
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada(){
+        this.referenciaEntrada = null;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<T>(conteudo);
+        if (this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i < this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
+    private No<T> getNo(int index){
+
+        valideIndice(index);
+
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for (int i = 0; i < this.size()-1; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    public T remove(int index){
+        No<T> noPivor = this.getNo(index);
+
+        if (index == 0){
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+            No<T> noAnterior = getNo(index -1);
+            noAnterior.setProximoNo(noPivor.getProximoNo());
+            return noPivor.getConteudo();
+
+        //Para os casos de index != 0, esse método irá "apontar" para o próximo No em questão (se o No for 3 por exemplo, irá fazer 3 - 1 = 2
+        // e irá apontar para o 4) e após perder a referência do 3 ele será automaticamente removido -- É como se ligase o 2 com o 4 e perdesse o 3)
+        //Esse processo é feito na JVM que irá "perceber" que o objeto 3 perdeu a referência
+    }
+
+    public int size(){
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true){
+            if (referenciaAux != null){
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo() != null){
+                    referenciaAux = referenciaAux.getProximoNo();
+                }else {
+                    break;
+                }
+            }else {
+                break;
+            }
+        }
+        return tamanhoLista;
+    }
+
+    private void valideIndice(int index){
+        if (index >= size()){
+            int ultimoIndice = size()-1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo no índice " + index + " desta lista. Esta lista só vai até o índice " + ultimoIndice + '.');
+        }
+    }
+
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    public boolean isEmpty(){
+        return referenciaEntrada == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i < this.size(); i++){
+            strRetorno += "[No{conteudo" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
