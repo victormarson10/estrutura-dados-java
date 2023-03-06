a0a276f869dc1af42af92bb448e328e22087a0be
victor10 <victormarson10@gmail.com>
2023-03-06 19:51:23 -0300

    frist change for implementation doble list chained

---
+++ estrutura-dados/src/main/java/com/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,71 @@
+package com.listaDuplamenteEncadeada;
+
+import com.listaencadeada.No;
+
+public class ListaDuplamenteEncadeada<T> {
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista;
+
+    public ListaDuplamenteEncadeada(){
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<T>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+
+        if (primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
+    public void add(int index, T elemento){
+        NoDuplo<T> noAuxiliar = getNo(index);
+        NoDuplo<T> novoNo = new NoDuplo<T>(elemento);
+        novoNo.setNoProximo(noAuxiliar);
+
+        if (novoNo.getNoProximo() != null){
+            novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        }else {
+            novoNo.setNoPrevio(ultimoNo);
+            ultimoNo = novoNo;
+        }
+
+        if (index == 0){
+            primeiroNo = novoNo;
+        }else {
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+        tamanhoLista++;
+
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
+    public int size(){
+        return tamanhoLista;
+    }
+}
