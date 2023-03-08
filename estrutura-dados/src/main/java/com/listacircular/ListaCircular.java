b7a7ae6fc244e4f9d6c696854134bf08676012a9
victor10 <victormarson10@gmail.com>
2023-03-08 20:48:03 -0300

    implementation of the circular list - frist commit

---
+++ estrutura-dados/src/main/java/com/listacircular/ListaCircular.java
@@ -0,0 +1,55 @@
+package com.listacircular;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeça;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    public void remove(int index){
+        if (index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista");
+
+            No<T> noAuxiliar = this.cauda;
+            if (index == 0){
+                this.cauda = this.cauda.getNoProximo();
+                this.cabeça .setNoProximo(this.cauda);
+        }else if (index == 1){
+                this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+            }else {
+                for (int i = 0; i < index-1; i++){
+                    noAuxiliar = noAuxiliar.getNoProximo();
+                }
+                noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+            }
+            this.tamanhoLista--;
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if ( this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+
+        if (index == 0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true : false;
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+}
