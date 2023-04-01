cb50afea3c034b550789c74752251e130e7cc93d
victor10 <victormarson10@gmail.com>
2023-04-01 20:17:07 -0300

    including class for structure tree binare

---
+++ estrutura-dados/src/main/java/com/arvoreBinaria/ArvoreBinaria.java
@@ -0,0 +1,66 @@
+package com.arvoreBinaria;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo){
+        BinNo<T> novoNo = new BinNo<T>(conteudo);
+        raiz = inserir(raiz, novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+        if (atual == null){
+            return novoNo;
+        }else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0){
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }else {
+            atual.setNoDir(inserir(atual.getNoEsq(), novoNo));
+        }
+        return atual;
+    }
+
+    public void exibirInOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+
+    private void exibirInOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
+
+    public void exibirPosOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirPosOrdem(this.raiz);
+    }
+
+    private void exibirPosOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo() + ", ");
+        }
+    }
+
+    public void exibirPreOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirPreOrdem(this.raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual){
+        if (atual != null){
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+        }
+    }
+
+}
